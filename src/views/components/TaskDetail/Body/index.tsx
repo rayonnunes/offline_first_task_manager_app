d4c96a2718773667ee54f9d01d6fdd596959f16e
rayonnunes <rayon.nunes@redeinova.net>
2020-12-09 10:09:56 -0300

    Added some types in withObservables

---
+++ src/views/components/TaskDetail/Body/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Body = () => {
+
+  return (
+    <></>
+  )
+}
+
+export default Body
--- src/views/components/TaskDetail/index.tsx
+++ src/views/components/TaskDetail/index.tsx
@@ -2,23 +2,29 @@ import React from 'react'
 import withObservables from '@nozbe/with-observables'
 import Container from './Container'
 import Header from './Header'
+import Body from './Body'
+import { TasksResponseProps } from '../../../store/modules/tasks/types'
 
-const Detail = { Container, Header }
+const Detail = { Container, Header, Body }
 
-const TaskDetail = ({ tasks }) => {
-  const { status, title }: { status: string; title: string } = tasks
+const TaskDetail = ({ tasks }: { tasks: TasksResponseProps }) => {
+  const { status, title } = tasks
 
   return (
     <Detail.Container>
       <Detail.Header status={status} title={title} />
+      <Detail.Body />
     </Detail.Container>
   )
 }
 
-const enhance = withObservables(['tasks'], ({ tasks }) => {
-  return {
-    tasks,
-  }
-})
+const enhance = withObservables(
+  ['tasks'],
+  ({ tasks }: { tasks: TasksResponseProps }) => {
+    return {
+      tasks,
+    }
+  },
+)
 
 export default enhance(TaskDetail)
